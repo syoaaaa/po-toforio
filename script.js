fc13509836c23b76afecc922478a619d50deac47
syoaaaa <syoukawa0814@gmail.com>
2024-05-27 23:45:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactive features using JavaScript here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+    // Additional JavaScript can be added here
+});
